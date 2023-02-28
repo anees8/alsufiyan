4e54d097127078374027a380a2ad29393d7b9a9d
anees8 <aneesahmed8484@gmail.com>
2023-02-28 18:48:37 +0530

    navbar

--- resources/js/Components/common/Navbar.vue
+++ resources/js/Components/common/Navbar.vue
@@ -1,4 +1,37 @@
 <template>
+  <!-- <b-navbar toggleable="lg" type="light" variant="white" fixed="top" class="p-0 m-0">
+    <b-navbar-brand href="#">
+      <img class="mx-1" :src="logo" height="48" /> <img :src="slogo" height="48"
+    /></b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item class="px-xl-3 px-md-2 py-2" :to="{ name: 'Home' }"
+          ><font-awesome-icon class="me-2" icon="home" />Home</b-nav-item
+        >
+        <b-nav-item class="px-xl-3 px-md-2 py-2" :to="{ name: 'About' }"
+          ><font-awesome-icon class="me-2" icon="address-card" />About</b-nav-item
+        >
+        <b-nav-item class="px-xl-3 px-md-2 py-2" :to="{ name: 'blog' }"
+          ><font-awesome-icon class="me-2" icon="blog" />Blog</b-nav-item
+        >
+        <b-nav-item class="px-xl-3 px-md-2 py-2" :to="{ name: 'gallery' }"
+          ><font-awesome-icon class="me-2" icon="image" />Gallery</b-nav-item
+        >
+        <b-nav-item class="px-xl-3 px-md-2 py-2" :to="{ name: 'Videos' }"
+          ><font-awesome-icon class="me-2" icon="video" />Videos</b-nav-item
+        >
+        <b-nav-item class="px-xl-3 px-md-2 py-2" :to="{ name: 'contact' }"
+          ><font-awesome-icon class="me-2" icon="envelope" />Contact</b-nav-item
+        >
+        <b-nav-item class="px-xl-3 px-md-2 py-2" :to="{ name: 'Login' }"
+          >Login<font-awesome-icon class="ms-2" icon="arrow-right"
+        /></b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar> -->
   <nav
     class="navbar navbar-expand-lg fixed-top navbar-light bg-white justify-content-between p-0 m-0"
   >
